6f3f5856d1f3d5e23657fde34dc510a290f0423c
yannsi <64046112+yannsi@users.noreply.github.com>
2024-03-24 11:26:30 +0900

    Update hyprlanddotfile.sh

--- hyprlanddotfile.sh
+++ hyprlanddotfile.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
 
 git clone https://github.com/yannsi/hyprland.git
-cd $HOME/hyprland/.config
-&&
-
-printf "password: "
+cd $HOME/hyprland/.config && printf "password: "
 read password
-echo "$password" | sudo -S pacman -S --needed - < pkglist_pacman_hypr.txt
-&&
-cp -r cava hypr waybar starship.toml $HOME/.config
-cd
-&&
-git clone https://aur.archlinux.org/cava.git
+echo "$password" | sudo -S pacman -S --needed - < pkglist_pacman_hypr.txt && cp -r cava hypr waybar starship.toml $HOME/.config
+cd && git clone https://aur.archlinux.org/cava.git
 cd $HOME/cava/ && makepkg -si && cd && rm -rf cava
 echo "設定が完了しました。再起動してください。"
 
