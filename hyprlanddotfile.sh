3575130b481b64c93cdf98e2172e59c1adeac0f5
yannsi <64046112+yannsi@users.noreply.github.com>
2024-03-24 10:40:15 +0900

    Add files via upload

---
+++ hyprlanddotfile.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+git clone https://github.com/yannsi/hyprland.git
+cd $HOME/hyprland/.config
+pacman -S --needed - < pkglist_pacman_hypr.txt
+cp -r cava hypr waybar starship.toml $HOME/.config
+cd
+
+git clone https://aur.archlinux.org/cava.git
+cd $HOME/cava/ && makepkg -si && cd && rm -rf cava
+echo "設定が完了しました。再起動してください。"
+
