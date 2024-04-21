74f97f413619f1bc9301cbe394b62626c31bd2d5
yannsi <64046112+yannsi@users.noreply.github.com>
2024-04-21 15:03:51 +0900

    Delete hyprlanddotfile.sh

--- hyprlanddotfile.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-git clone https://github.com/yannsi/hyprland.git
-echo -e "\n次のコマンドを実行してください。\n\ncd $HOME/hyprland/.config\nsudo pacman -S --needed - < pkglist_pacman_hypr.txt\ncp -r cava hypr waybar starship.toml $HOME/.config\ncd .. && cp .kannsuu.txt $HOME\nstarship preset tokyo-night -o ~/.config/starship.toml\ncd && git clone https://aur.archlinux.org/cava.git\ncd $HOME/cava/ && makepkg -si\ncd && rm -rf cava\n"
