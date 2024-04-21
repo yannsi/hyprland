1e337354deb394e11512c97d801e4c4f4ade0de4
yannsi <64046112+yannsi@users.noreply.github.com>
2024-04-21 10:21:59 +0900

    Add files via upload

---
+++ test.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+set -e
+
+
+git clone https://github.com/yannsi/hyprland.git
+
+echo -e "\nパッケージのインストールと設定を行います。\nパスワードを入力してください。\n"
+sudo chmod +x $HOME/hyprland/hyprlanddotfile.sh
+
+cd $HOME/hyprland/.config
+
+sudo pacman -S --needed - < pkglist_pacman_hypr.txt
+wait
+
+cp -r hypr waybar starship.toml $HOME/.config
+
+cd .. && cp .kannsuu.txt $HOME/
+
+starship preset tokyo-night -o ~/.config/starship.toml
+
+cd && git clone https://aur.archlinux.org/cava.git
+cd $HOME/cava/ && makepkg -si
+wait
+cd && rm -rf cava
+cd $HOME/hyprland/.config
+cp -r cava $HOME/.config && cd
+
+git clone https://aur.archlinux.org/flameshot-git.git
+cd $HOME/flameshot-git/ && makepkg -si
+wait
+cd && rm -rf flameshot-git
+
+git clone https://aur.archlinux.org/hyprpicker-git.git
+cd $HOME/hyprpicker-git && makepkg -si
+wait
+cd && rm -rf hyprpicker-git
+
+git clone https://aur.archlinux.org/hyprshot.git
+cd $HOME/hyprshot && makepkg -si
+wait
+cd && rm -rf hyprshot
+
+echo -e "GTK_IM_MODULE=fcitx\nQT_IM_MODULE=fcitx\nXMODIFIERS=@im=fcitx" >> /etc/environment
+
+echo ". .kannsuu.txt" >> .bashrc
+
+echo "終了しました。再起動してください。"
+
