9cc6eb33b84a3fe925d21046d05a6bbc6eab0cc1
yannsi <64046112+yannsi@users.noreply.github.com>
2024-05-12 15:36:19 +0900

    Update wallchange.sh

--- .config/waybar/scripts/wallchange.sh
+++ .config/waybar/scripts/wallchange.sh
@@ -6,7 +6,7 @@ if [ $? -eq 1 ]; then
 exit 0
 fi
 
-wall=$(zenity --file-selection --filename="$HOME/" --text="壁紙を選んでください")
+wall=$(zenity --file-selection --filename=$HOME/ --text="壁紙を選んでください")
 
 if [ -n "$wall" ]; then
  echo '#!/bin/bash' > $HOME/.config/waybar/scripts/wallpaper
